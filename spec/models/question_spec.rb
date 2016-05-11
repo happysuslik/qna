fbc52222e345576b9b1baa75e7ae11c9942fb8d0
happysuslik <happysuslikrealist@gmail.com>
2016-05-11 19:24:54 +0300

    add association between question and answers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,14 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it 'validate presence of title' do
-    expect(Question.new(body: 'Qwerr')).to_not be_valid
-  end
-
-  it 'validate presence of body' do
-    expect(Question.new(title: 'Qwerr')).to_not be_valid
-  end
-
+  it { should have_many :answers }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 
